00932768cb2886d6128d41b028ae5367531a6556
Amy Parker <amy@amyip.net>
2023-12-02 15:41:42 -0800

    first working version
    This patch submits the first working version of the library. It is not ready for release yet, but it can operate correctly with the test server.
    
    Signed-off-by: Amy Parker <amy@amyip.net>

---
+++ tests/test1.py
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import os
+import asyncio
+import math
+
+import libacmchristmas
+
+import numpy as np
+
+
+async def main():
+    tree = libacmchristmas.TreeController(url=os.environ["TEST_URL"])
+    await tree.connect()
+    for n in range(20):
+        tree.leds[n] = 0x888800 + 8*n
+    await tree.update_manual()
+    await asyncio.sleep(3)
+    await tree.clear()
+    await asyncio.sleep(3)
+    img = np.array(object=[], dtype=np.uint8)
+    for x in range(tree.ix):
+        for y in range(tree.iy):
+            c = math.floor(((x+y)/(tree.ix+tree.iy))*256)
+            img = np.concatenate((img, np.array([c, c, c, 0], dtype=np.uint8)))
+    await tree.update_image(img)
+    
+    
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+loop.run_until_complete(main())    
