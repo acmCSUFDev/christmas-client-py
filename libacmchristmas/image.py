f96af3f438911cad74a0f5c8d0dcc9f3c1401f9f
Amy Parker <amy@amyip.net>
2023-12-02 19:30:11 -0800

    Image.Image
    Use Image.Image instead of Image, fixing types
    
    Signed-off-by: Amy Parker <amy@amyip.net>

--- libacmchristmas/image.py
+++ libacmchristmas/image.py
@@ -5,15 +5,15 @@ from PIL import Image
 
 
 # Assumes size is already correct
-def prepare_image(img: Image) -> np.array:
+def prepare_image(img: Image.Image) -> np.array:
     return np.array(img.convert("RGBA")).ravel()
 
 
-def resize_image(img: Image, ix: int, iy: int) -> Image:
+def resize_image(img: Image.Image, ix: int, iy: int) -> Image:
     return img.resize((ix, iy))
 
 
-def load_direct(img: Image, ix: int, iy: int) -> np.array:
+def load_direct(img: Image.Image, ix: int, iy: int) -> np.array:
     return prepare_image(resize_image(img, ix, iy))
 
 
